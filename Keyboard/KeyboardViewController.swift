d1d3678238e7b57f035991fece24834de5d91428
Altynbek Usenov <altynbek.usenov@pepperstone.com>
2016-07-26 16:31:41 +1000

    Kyrgyz flag added

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -35,7 +35,7 @@ class KeyboardViewController: UIInputViewController, CustomKeyboardDelegate {
 
     // This is how you add extra buttons to layouts for customising CustomKeyboard without even subclass it!
     let customButton = KeyboardButton(
-      type: .Text("🕶"),
+      type: .Text("🇰🇬"),
       style: CustomKeyboardKeyButtonStyle,
       width: .Static(width: 40),
       identifier: "customButton")
@@ -45,36 +45,36 @@ class KeyboardViewController: UIInputViewController, CustomKeyboardDelegate {
       buttonIndex: 2)
 
     // This is how you add a menu 
-    let globeButtons = [
-      customKeyboard.keyboardLayout.uppercase.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
-      customKeyboard.keyboardLayout.uppercaseToggled.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
-      customKeyboard.keyboardLayout.lowercase.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
-      customKeyboard.keyboardLayout.numbers.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
-      customKeyboard.keyboardLayout.symbols.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
-    ]
-
-    let menuItemStyle =  KeyMenuItemStyle(
-      separatorColor: UIColor(red: 210.0/255.0, green: 213.0/255.0, blue: 219.0/255.0, alpha: 1),
-      separatorWidth: 0.5)
-
-    for globeButton in globeButtons {
-      let menu = KeyMenu(
-        items: [
-          KeyMenuItem(title: "Switch Keyboard", style: menuItemStyle, action: { _ in self.advanceToNextInputMode() }),
-          KeyMenuItem(title: "Settings", style: menuItemStyle, action: { _ in print("settings pressed") }),
-          KeyMenuItem(title: "About Us", style: menuItemStyle, action: { _ in print("about pressed") }),
-        ],
-        style: KeyMenuStyle(itemSize: CGSize(width: 150, height: 40)),
-        type: .Vertical)
-      globeButton?.keyMenu = menu
-    }
+//    let globeButtons = [
+//      customKeyboard.keyboardLayout.uppercase.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
+//      customKeyboard.keyboardLayout.uppercaseToggled.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
+//      customKeyboard.keyboardLayout.lowercase.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
+//      customKeyboard.keyboardLayout.numbers.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
+//      customKeyboard.keyboardLayout.symbols.getKeyboardButton(atRowIndex: 3, buttonIndex: 1),
+//    ]
+//
+//    let menuItemStyle =  KeyMenuItemStyle(
+//      separatorColor: UIColor(red: 210.0/255.0, green: 213.0/255.0, blue: 219.0/255.0, alpha: 1),
+//      separatorWidth: 0.5)
+//
+//    for globeButton in globeButtons {
+//      let menu = KeyMenu(
+//        items: [
+//          KeyMenuItem(title: "Switch Keyboard", style: menuItemStyle, action: { _ in self.advanceToNextInputMode() }),
+//          KeyMenuItem(title: "Settings", style: menuItemStyle, action: { _ in print("settings pressed") }),
+//          KeyMenuItem(title: "About Us", style: menuItemStyle, action: { _ in print("about pressed") }),
+//        ],
+//        style: KeyMenuStyle(itemSize: CGSize(width: 150, height: 40)),
+//        type: .Vertical)
+//      globeButton?.keyMenu = menu
+//    }
   }
 
   // MARK: CustomKeyboardDelegate
   func customKeyboard(customKeyboard: CustomKeyboard, keyboardButtonPressed keyboardButton: KeyboardButton) {
     if customKeyboard == self.customKeyboard {
       if keyboardButton.identifier == "customButton" {
-        print("custom button pressed")
+        textDocumentProxy.insertText("Алга Кыргызстан!")
       }
     }
   }
@@ -100,6 +100,12 @@ class KeyboardViewController: UIInputViewController, CustomKeyboardDelegate {
   func customKeyboardReturnButtonPressed(customKeyboard: CustomKeyboard) {
     if customKeyboard == self.customKeyboard {
       textDocumentProxy.insertText("\n")
+    }
+  }
+    
+  func customKeyboardGlobeButtonPressed(customKeyboard: CustomKeyboard) {
+    if customKeyboard == self.customKeyboard {
+      advanceToNextInputMode()
     }
   }
 }
